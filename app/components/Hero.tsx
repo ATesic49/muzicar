2c077dbf49e67e8a9bd7c9ba6f3405a84984071b
ATesic49 <atesic7@gmail.com>
2025-06-04 03:18:46 +0200

    das

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,17 +6,25 @@ import slika3 from "@/public/imgs/slika3.jpg";
 import slika4 from "@/public/imgs/slika2.jpg";
 const Hero = () => {
 	const list = [
-		{ ime: "S l i k e", slika: slika },
-		{ ime: "S n i m c i", slika: slika2 },
-		{ ime: "R E P E R T O A R", slika: slika4 },
-		{ ime: "K o n t a k t", slika: slika3 },
+		{ ime: "S l i k e", slika: slika, id: "slike" },
+		{ ime: "S n i m c i", slika: slika2, id: "snimci" },
+		{ ime: "R E P E R T O A R", slika: slika4, id: "repertoar" },
+		{ ime: "K o n t a k t", slika: slika3, id: "kontakt" },
 	];
+
+	const handleScrollToElement = (id: string) => {
+		const kontaktElement = document.getElementById(id);
+		if (kontaktElement) {
+			kontaktElement.scrollIntoView({ behavior: "smooth" });
+		}
+	};
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-4 px-0 gap-2 justify-center w-full bg-neutral-900 min-h-screen overflow-hidden">
-			{list.map((li, i) => {
+			{list.map((li) => {
 				return (
 					<div
-						key={i}
+						key={li.id}
+						onClick={() => handleScrollToElement(li.id)}
 						className="relative bg-red-200 w-full flex justify-center items-center rotate-6 min-h-[120%] -translate-y-[10%]  overflow-hidden rounded-xl h-full   cursor-pointer hover:scale-105 duration-200"
 					>
 						<Image
